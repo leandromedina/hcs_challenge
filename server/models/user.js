8ed4c7070550a80ef9273b6046a11abca18b2cc4
Leandro Medina <leandromedinacortes@gmail.com>
2019-08-05 23:05:02 -0300

    user model methods

--- server/models/user.js
+++ server/models/user.js
@@ -20,3 +20,31 @@ const userSchema = mongoose.Schema({
 })
 
 const User = (module.exports = mongoose.model("User", userSchema))
+
+module.exports.loginUser = (username, password, callback) => {
+  const conditions = { username, password }
+  User.findOne(conditions, callback)
+}
+
+module.exports.getUsers = (callback, limit) => {
+  User.find(callback).limit(limit)
+}
+
+module.exports.getUser = (id, callback) => {
+  User.findById(id, callback)
+}
+
+module.exports.createUser = (user, callback) => {
+  User.create(user, callback)
+}
+
+module.exports.modifyUser = (id, user, options, callback) => {
+  const update = {
+    name: user.name
+  }
+  User.findByIdAndUpdate(id, update, options, callback)
+}
+
+module.exports.deleteUser = (id, callback) => {
+  User.findByIdAndDelete(id, callback)
+}
